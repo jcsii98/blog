8be86923bfe7541acbd3b62816d6a122c202bb0a
Jose Saribong <jose.c.saribong@gmail.com>
2023-07-03 20:41:13 +0800

    generate model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_02_074458) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_120919) do
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
