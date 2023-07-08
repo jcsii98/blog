b2e5199c5aa477e05c211dd26138fbcccb41d4cf
Jose Saribong <jose.c.saribong@gmail.com>
2023-07-08 12:19:40 +0800

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_093647) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_07_055730) do
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "category"
   end
 
   create_table "posts", force: :cascade do |t|
