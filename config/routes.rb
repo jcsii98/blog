8e4ae27e0c8dd0587569aaaf3f68262b83e5ba75
Jose Saribong <jose.c.saribong@gmail.com>
2023-07-08 12:48:46 +0800

    css compressor issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   resources :posts
-
+  resources :articles
   root 'articles#index'
-  # localhost:3000/articles
+  # # localhost:3000/articles
 
-  get '/articles' => 'articles#index' 
-  get '/articles/new' => 'articles#new', as: 'new_article'
+  # get '/articles' => 'articles#index' 
+  # get '/articles/new' => 'articles#new', as: 'new_article'
   
 
-  get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
+  # get '/articles/:id/edit' => 'articles#edit', as: 'edit_article'
 
-  # localhost:3000/articles/new
-  post '/articles' => 'articles#create', as: 'create_article'
-  get '/articles/:id' => 'articles#show', as: 'show_article'
-  patch '/articles/:id' => 'articles#update', as: 'update_article'
+  # # localhost:3000/articles/new
+  # post '/articles' => 'articles#create', as: 'create_article'
+  # get '/articles/:id' => 'articles#show', as: 'show_article'
+  # patch '/articles/:id' => 'articles#update', as: 'update_article'
 
-  delete '/articles/:id' => 'articles#delete', as: 'delete_article'
+  # delete '/articles/:id' => 'articles#delete', as: 'delete_article'
 end
